90a630bd3421de72a2f044a699e49c03e7652845
CyberMage7 <technician9012@gmail.com>
2024-06-07 16:00:35 +0530

    Updated the images

--- vlsi-fab/src/header/Header.js
+++ vlsi-fab/src/header/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Logo from '../assets/logo_jiit.png';
+import Head from '../assets/header_pic.jpg';
 import "./header.css";
 
 export default function Header() {
@@ -6,15 +8,15 @@ export default function Header() {
     <div className="head">
       <div className="head-inside">
         <div className="logo_jiit">
-          <img src="./assets/logo_jiit.png" alt=""></img>
+          <img src={Logo} alt="JIIT" className="logo"></img>
         </div>
         <div className="content">
           <h1>VLSI DESIGN, FABRICATION AND</h1>
-          <h1>CHARACTERIZATION fACULTY</h1>
+          <h1>CHARACTERIZATION FACULTY</h1>
           <h2>Jaypee Institute of Information and Technology</h2>
         </div>
         <div className="head_pic">
-        <img src="./assets/header_pic.jpg" alt=""></img>
+        <img src={Head} alt="Students" className="stud"></img>
         </div>
       </div>
     </div>
