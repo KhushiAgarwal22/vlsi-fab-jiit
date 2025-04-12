991cb8949ce9647a4e0a5137896ee0bdad02146a
Abhinav Mishra <mishraabhinav2306@gmail.com>
2025-04-12 10:49:22 +0530

    facilities changes

--- src/credits/Credit.js
+++ src/credits/Credit.js
@@ -7,7 +7,17 @@ export default function Credit() {
         <h2>Copyright 2024 © JIIT</h2>
       </div>
       <div>
-        <p>Designed and developed by <a href="https://www.linkedin.com/in/khushi-agarwal-588990267/">Khushi Agarwal</a>, <a href="https://www.linkedin.com/in/vishwas-mishra-4b6315257/">Vishwas Mishra</a></p>
+        <p>
+          Designed and developed by{" "}
+          <a href="https://www.linkedin.com/in/khushi-agarwal-588990267/">
+            Khushi Agarwal
+          </a>
+          ,{" "}
+          <a href="https://www.linkedin.com/in/vishwas-mishra-4b6315257/">
+            Vishwas Mishra
+          </a>
+          , <a href="https://www.linkedin.com/in/abhinavm97/">Abhinav Mishra</a>
+        </p>
       </div>
     </div>
   );
